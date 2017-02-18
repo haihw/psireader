b4d20beed006090abdcfb8b3d941c25294f4d613
haihw <nguyenhuuhiephai@gmail.com>
2017-02-18 15:09:13 +0800

    Initial Commit

---
+++ PSIReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PSIReader
+//
+//  Created by Hai Hw on 18/2/17.
+//  Copyright © 2017 Hai Hw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
